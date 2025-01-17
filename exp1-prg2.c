630de356d82255e57fc936d1e1213d3229fad0d8
Student <50114839+SentientToast1@users.noreply.github.com>
2025-01-17 17:30:56 +0530

    --

--- exp1-prg2.c
+++ exp1-prg2.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main(){
+    int distInKM,distInCM,distInIN,distInFT;
+    printf("Enter distance between cities in km: ");
+    scanf("%d",&distInKM);
+    distInCM = distInKM * 100000;
+    distInFT = distInKM *  3281;
+    distInIN = distInFT * 12;
 
-
-
-
+    print
     return 0;
 }
