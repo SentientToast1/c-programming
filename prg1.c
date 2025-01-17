28f0476b2ed0ae5b76095e3c244f15f28f55dc17
SentientToast1 <50114839+SentientToast1@users.noreply.github.com>
2025-01-17 13:44:30 +0530

    hh

---
+++ prg1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int a = 55;
+    int b;
+    printf("Enter num: ");
+    scanf("%d", &b);
+    printf("sum = %d\n", a+b);
+    return 0;
+}
---
+++ prg2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int age1, age2, age3;
+    printf("Enter Ram's age: ");
+    scanf("%d",age1);
+    printf("Enter Sham's age: ");
+    scanf("%d",age2);
+    printf("Enter Ajay's age: ");
+    scanf("%d",age3);
+
+    return 0;
+}
