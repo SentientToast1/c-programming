924fcec294e2f249425bf012fa0486bd7762667d
sentienttoast1 <50114839+SentientToast1@users.noreply.github.com>
2025-01-23 12:47:54 +0530

    %DATE%

--- 23-01.c
+++ 23-01.c
@@ -1,44 +1,102 @@
 #include<stdio.h>
-int main(){
-//     int num;
-//     printf("enter a number: ");
-//     scanf("%d", &num);
+int main()
+{
+//    int num;
+//    printf("enter a number: ");
+//    scanf("%d", &num);
 //
-//     if(num % 2 == 0){
-//          printf("is even number");
-//     }
-//     else{
-//          printf("is odd number");
-//     }
-
-
-//     int age;
-//     printf("enter your age: ");
-//     scanf("%d", &age);
+//    if(num % 2 == 0)
+//    {
+//        printf("is even number");
+//    }
+//    else
+//    {
+//        printf("is odd number");
+//    }
 //
-//     if(age < 18){
-//          printf("minor and not eligible");
-//     }
-//     else if(age >= 18 && age >=60 ){
-//          printf("senior and not eligible");
-//     }
-//     else{
-//          printf("you can work");
+//
+//    int age;
+//    printf("enter your age: ");
+//    scanf("%d", &age);
+//
+//    if(age < 18)
+//    {
+//        printf("minor and not eligible");
+//    }
+//    else if(age >= 18 && age >=60 )
+//    {
+//        printf("senior and not eligible");
+//    }
+//    else
+//    {
+//        printf("you can work");
+//    }
+//
+//    int marks;
+//    printf("enter your marks: ");
+//    scanf("%d",&marks);
+//    if(marks>90)
+//    {
+//        printf("A grade");
+//    }
+//    else if(marks >= 70 && marks <= 90)
+//    {
+//        printf("B grade");
+//    }
+//    else if(marks >= 50 && marks < 70)
+//    {
+//        printf("C grade");
+//    }
+//    else
+//    {
+//        printf("Fail");
+//    }
+
+//     char operation;
+//     double first, second;
+//     printf("Enter an operator: ");
+//     scanf("%c", &operation);
+//     printf("enter 2 operands: ");
+//     scanf("%lf %lf",&first,&second);
+//     switch(operation){
+//     case '+':
+//          printf("%lf + %lf = %lf",first, second, first+second);
+//          break;
+//     case '-':
+//          printf("%lf - %lf = %lf",first, second, first-second);
+//          break;
+//     default:
+//          printf("wrong operator!");
+//
+//
 //     }
 
-     int marks;
-     printf("enter your marks: ");
-     scanf("%d",&marks);
-     if(marks>90){
-          printf("A grade");
+
+     char letter;
+     printf("enter: ");
+     scanf("%c",&letter);
+     switch(letter){
+     case 'a':
+          printf("vowel is a");
+          break;
+     case 'e':
+          printf("vowel is e");
+          break;
+     case 'i':
+          printf("vowel is i");
+          break;
+     case 'o':
+          printf("vowel is o");
+          break;
+     case 'u':
+          printf("vowel is u");
+          break;
+     default:
+          printf("not a vowel");
      }
-     else if(marks >= 70 && marks <= 90){
-          printf("B grade");
-     }
-     else if(marks >= 50 && marks < 70){
-          printf("C grade");
-     }
-     else{
-          printf("Fail");
-     }
+
+
+
+     return 0;
+
 }
