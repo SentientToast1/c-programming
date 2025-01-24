5f53dee5a590b60badd39d2a8f5c5527d2cf2dba
sentienttoast1 <50114839+SentientToast1@users.noreply.github.com>
2025-01-24 16:59:12 +0530

    --

--- 24-01.c
+++ 24-01.c
@@ -1,15 +1,30 @@
 #include<stdio.h>
-int main(){
-    int i,n;
-    printf("enter range: ");
-    scanf("%d",&n);
-    i = 2;
-    printf("%d",i);
-    while(i <= n){
-        printf(" %d",i*2);
+void main(){
 
-        i= i + 1;
-    }
-    return 0;
+/*
+int i,n;
+printf("enter range: ");
+scanf("%d",&n);
+i = 2;
+printf("%d",i);
+while(i <= n){
+    printf(" %d",i*2);
+
+    i= i + 1;
+}
+*/
+
+/*
+int num,sum;
+printf("Enter number: ");
+scanf("%d",&num);
+sum = 0;
+while(num>0){
+    sum = sum + (num%10);
+    num = num/10;
+}
+printf("the sum is: %d",sum);
+*/
+
 
 }
